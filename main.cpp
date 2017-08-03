aa8cf4e00003973f17cb4d174eea55820a9c255e
dennisalejandro <dennisalenjandro@unitec.edu>
2017-08-03 12:15:53 -0600

    prueba de git

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void matrizMet(int**, int);
+int main() {
+	int const l = 10;
+	int** matriz = new int*[l];
+	for(int i=0;i<l;i++){
+		matriz[i]= new int[l];
+	}
+	for (int i = 0;i < l;i++) {
+		matriz[0][i] = i;
+	}
+	for (int i = 0;i < l;i++) {
+	cout << matriz[0][i];
+	}
+	matrizMet(matriz, l);
+	for (int i = 0;i < l;i++) {
+		cout << matriz[0][i] << endl;
+	}
+	return 0;
+}
+void matrizMet(int** matriz,int l) {
+	for (int i = 0;i<l;i++) {
+		matriz[0][i] = 0;
+	}
+}
+
